58aa7ba0e351997739d537acd8e7fc9071479f6c
Luna <rylanhughey@gmail.com>
2025-06-21 15:49:41 -0500

    Fix extension files

--- background.js
+++ background.js
@@ -1,32 +1,31 @@
-chrome.action.onClicked.addListener(() => {
-    // Get CPU information
-    chrome.system.cpu.getInfo((cpuInfo) => {
-      // Get Memory information
-      chrome.system.memory.getInfo((memoryInfo) => {
-        // Get Storage information
-        chrome.system.storage.getInfo((storageInfo) => {
-          // Create a data object with all system info
-          const systemInfo = {
-            cpu: cpuInfo,
-            memory: memoryInfo,
-            storage: storageInfo
-          };
-  
-          // Convert the data object to a JSON string
-          const jsonString = JSON.stringify(systemInfo, null, 2);
-  
-          // Save the JSON string to a file
-          const blob = new Blob([jsonString], { type: 'application/json' });
-          const url = URL.createObjectURL(blob);
-  
-          // Trigger the download of the file
-          chrome.downloads.download({
-            url: url,
-            filename: 'system-info.json',
-            saveAs: true
-          });
+function collectSystemInfo() {
+  chrome.system.cpu.getInfo((cpuInfo) => {
+    chrome.system.memory.getInfo((memoryInfo) => {
+      chrome.system.storage.getInfo((storageInfo) => {
+        const systemInfo = {
+          cpu: cpuInfo,
+          memory: memoryInfo,
+          storage: storageInfo,
+        };
+
+        const jsonString = JSON.stringify(systemInfo, null, 2);
+        const blob = new Blob([jsonString], { type: 'application/json' });
+        const url = URL.createObjectURL(blob);
+
+        chrome.downloads.download({
+          url,
+          filename: 'system-info.json',
+          saveAs: true,
         });
       });
     });
   });
-  
+}
+
+chrome.action.onClicked.addListener(collectSystemInfo);
+
+chrome.runtime.onMessage.addListener((request) => {
+  if (request.action === 'collectSystemInfo') {
+    collectSystemInfo();
+  }
+});
